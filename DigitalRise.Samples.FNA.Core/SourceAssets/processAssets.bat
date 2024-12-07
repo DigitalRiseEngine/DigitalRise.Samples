48974babefb7bc55aad22cd20614e3201a345352
Roman Shapiro <eleroman@mail.ru>
2024-12-07 07:51:28 +0700

    More assets

---
+++ DigitalRise.Samples.FNA.Core/SourceAssets/processAssets.bat
@@ -0,0 +1,3 @@
+drtexconv RustyCube\RustyMetal01_diffuse.png -o ..\Assets\Models\RustyMetal01_diffuse.dds
+drtexconv RustyCube\RustyMetal01_normal.png -o ..\Assets\Models\RustyMetal01_normal.dds --format normalInvertY
+drtexconv RustyCube\RustyMetal01_specular.png -o ..\Assets\Models\RustyMetal01_specular.dds
